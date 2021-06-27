b0e1c3eb04509842d0429315a0df27f9fb1ce7af
Bob <211xyz@163.com>
2021-06-27 10:05:58 +0800

    init(xlassloader): 初始化

---
+++ xlassloader/src/test/java/com/homework/xlassloader/XlassLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.xlassloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XlassLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
